e9456a108ac0f91a29ded33cb1786ee013c6cbd1
Prashant-2103 <153978892+Prashant-2103@users.noreply.github.com>
2024-04-07 07:07:25 +0000

    convertion of data types

---
+++ basics1/conversion.js
@@ -0,0 +1,12 @@
+let number = 12
+console.log(typeof number);
+
+let stringNum = String(number)
+console.log(stringNum);
+console.log(typeof stringNum);
+
+let name1 = "saira"
+console.log(typeof name1);
+let conName1 = Number(name1)
+console.log(typeof conName1);
+console.log( conName1);
