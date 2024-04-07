db1c7e7c223d1451f4fddf87539faf9ca2478605
Prashant-2103 <153978892+Prashant-2103@users.noreply.github.com>
2024-04-07 10:59:50 +0000

    some data types bro

---
+++ basics1/dataTypes1.js
@@ -0,0 +1,20 @@
+let temperature = null
+let someThing = undefined
+let thisThing = 0
+let thisThing2 = "0"
+
+let userName = "Sakshi Singh"
+
+let userAge = "21"
+
+let userPassword = "nodelbclbd"
+
+console.table([temperature, userName, userAge, userPassword]);
+
+console.log(typeof userName )
+console.log(typeof userAge )
+console.log(typeof userPassword )
+console.log(typeof temperature )
+console.log(typeof someThing )
+console.log(typeof thisThing )
+console.log(typeof thisThing2 )
