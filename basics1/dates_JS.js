b5f4ebac4cbf9aa3e1d38485bdf60d3f0a1af9bd
Prashant-2103 <153978892+Prashant-2103@users.noreply.github.com>
2024-04-11 03:53:00 +0000

    date and time

---
+++ basics1/dates_JS.js
@@ -0,0 +1,16 @@
+ const myDate = new Date()
+ console.log(myDate);
+ console.log(myDate.toDateString());
+ console.log(myDate.toISOString());
+ console.log(myDate.toJSON());
+ console.log(myDate.toLocaleDateString());
+ console.log(myDate.toLocaleString());
+ console.log(myDate.toString());
+ console.log(myDate.toTimeString());
+
+
+ console.log(typeof(myDate))
+
+
+ let myCreatedDate = new Date(2022,12,23)
+ console.log(myCreatedDate.toDateString())
